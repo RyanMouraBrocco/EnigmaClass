cf408caea48550df2140cad99dbb11c771fc2f26
RyanMouraBrocco <45888057+RyanMouraBrocco@users.noreply.github.com>
2024-10-27 12:49:32 -0300

    Update Banco.cs

--- Classes DAL/V9.2/EnigmaClass.CRUD/EnigmaClass.CRUD/Banco.cs
+++ Classes DAL/V9.2/EnigmaClass.CRUD/EnigmaClass.CRUD/Banco.cs
@@ -15,7 +15,7 @@ namespace EnigmaClass.CRUD
         /// <returns> Retorna um Sqlconnection Com a conexão já Aberta </returns>
         public static  SqlConnection Abrir()
         {
-            SqlConnection cn = new SqlConnection(@"Server=5c37e073-a92f-4ca6-a853-a8810009884b.sqlserver.sequelizer.com;Database=db5c37e073a92f4ca6a853a8810009884b;User ID=dfjaozkaqpvneyhg;Password=tKAuy4HAHwTLguvfHqNbj8gfMxk3m5q68uXyzrzkL5ymmU3soS54rpguDuQbSNpM;");
+            SqlConnection cn = new SqlConnection(@"CONNECTION-STRING-HERE");
             cn.Open();
             return cn;
         }
